4674b321229701f1ed1abc7f56439efac35efb25
jackwhihongtan <18tanj98@millshighsch.org>
2018-02-25 08:54:37 -0800

    Jack Tan

--- Prisms/Sphere.java
+++ Prisms/Sphere.java
@@ -1,4 +1,13 @@
 
 public class Sphere extends ThreeDShape {
-
+	private int radius;
+		public Sphere(int radius) {
+			this.radius = radius;
+		}
+		public double calcVolume() {
+			return 4 /3 * 3.1415 * radius * radius * radius;
+		}
+		public double calcSA() {
+			return 4 /3 * 3.1415 * radius * radius;
+		}
 }

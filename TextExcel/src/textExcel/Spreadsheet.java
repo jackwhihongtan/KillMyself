5f689b96d1a517e856d6729f585a76bdfce6bdcc
jackwhihongtan <18tanj98@millshighsch.org>
2018-03-14 21:31:27 -0700

    asdf

--- TextExcel/src/textExcel/Spreadsheet.java
+++ TextExcel/src/textExcel/Spreadsheet.java
@@ -3,66 +3,84 @@ package textExcel;
 
 // Update this file with your own code.
 
-public class Spreadsheet implements Grid
-{
+public class Spreadsheet implements Grid {
 	private Cell[][] elements;
+
 	public Spreadsheet() {
 		elements = new Cell[20][12];
-			for(int i = 0; i < 20; i++) {
-				for(int j = 0; j < 12; j++) {
-					elements[i][j] = new EmptyCell();
-				}
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < 12; j++) {
+				elements[i][j] = new EmptyCell();
 			}
-	}
-	@Override
-	public String processCommand(String command)
-	{
-		return "";
+		}
 	}
 
 	@Override
-	public int getRows()
+	public String processCommand(String command) // Parse and check how many chacreters are in it
 	{
+		String[] commands = command.split(" "); // Split to see how many are worsd are in String command
+		if (commands.length > 1 && commands[0] == "clear") {
+			int colNumber = Character.toUpperCase(command.charAt(0) - 'A');
+			int rowNumber = (Integer.valueOf(command.substring(1)) - 1); 
+			elements[colNumber][rowNumber] = new EmptyCell();
+			return getGridText();
+		} else if (commands[0] == "clear") { //Regular clear
+			for (int i = 0; i < 20; i++) {
+				for (int j = 0; j < 12; j++) {
+					elements[i][j] = new EmptyCell();
+				}
+			}
+			return getGridText();
+		} else if (commands.length == 1) { // Cell Inspection
+			return " \" " + command + " \" "; // This could need changing it feels wrong
+		} else { //Assiagment of String Values
+			int colNumber = Character.toUpperCase(command.charAt(0) - 'A');
+			int rowNumber = (Integer.valueOf(command.substring(1)) - 1); 
+			elements[colNumber][rowNumber] = new TextCell(commands[2]);
+			return getGridText();
+		}
+	}
+
+	@Override
+	public int getRows() {
 		// TODO Auto-generated method stub
 		return 20;
 	}
 
 	@Override
-	public int getCols()
-	{
+	public int getCols() {
 		// TODO Auto-generated method stub
 		return 12;
 	}
 
 	@Override
-	public Cell getCell(Location loc) //Returns a Cell at the location
+	public Cell getCell(Location loc) // Returns a Cell at the location
 	{
 		return elements[loc.getCol()][loc.getRow()];
 	}
 
 	@Override
-	public String getGridText()
-	{
+	public String getGridText() {
 		String entireSpreadSheet = "";
-		for(int i = 0; i < 21; i++) { //This for loops creates the numbers on the side
-			if(i == 0) { 
+		for (int i = 0; i < 21; i++) { // This for loops creates the numbers on the side
+			if (i == 0) {
 				entireSpreadSheet += "   |";
-			}else if(i <= 9) {
-				entireSpreadSheet += ""+i+"  |";
+			} else if (i <= 9) {
+				entireSpreadSheet += "" + i + "  |";
 			} else {
-				entireSpreadSheet += ""+i+" |";
+				entireSpreadSheet += "" + i + " |";
 			}
-			for(int j = 0; j < 12; j++) {
-				if(i == 0) { //Prints the Letters in the first row
-				entireSpreadSheet += (""+ (char) (65 + i))+"        |";
-				} else {//Prints the rest of the empty bracket
+			for (int j = 0; j < 12; j++) {
+				if (i == 0) { // Prints the Letters in the first row
+					entireSpreadSheet += ("" + (char) (65 + j)) + "        |";
+				} else {// Prints the rest of the empty bracket
 					entireSpreadSheet += "         |";
 				}
 			}
-			entireSpreadSheet = entireSpreadSheet + "\n"; //I need help inorder to print a new line afte ri print one row
-	}
-		System.out.println(entireSpreadSheet);
+			entireSpreadSheet = entireSpreadSheet + "\n"; // I need help inorder to print a new line afte ri print one
+															// row
+		}
 		return entireSpreadSheet;
-		
+
 	}
 }

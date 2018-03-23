503f426adb2f53e6c43645b020fa3c61a3e6a6c5
jackwhihongtan <18tanj98@millshighsch.org>
2018-03-23 00:03:43 -0700

    Did some progress

--- TextExcel/src/textExcel/PercentCell.java
+++ TextExcel/src/textExcel/PercentCell.java
@@ -11,21 +11,14 @@ public class PercentCell extends RealCells{
 	}
 	public String abbreviatedCellText() { //May need to change so it print out something same with Full Cell Text
 		int extraSpaces = 0; // To count how many spaces I need to add
-		String returnString = userInput;	
-			if (userInput.length() >= 10) { //Current method is broken
-				String[] decimal = userInput.split(".");
-				return ""+decimal[0]+"%";
-			} else {
-				String[] decimal = userInput.split(".");
-				returnString = ""+decimal[0]+"%";
-				extraSpaces = 10 - returnString.length();
-				for (int j = 0; j < extraSpaces; j++) {
-					returnString += " ";
-				}
-				return returnString;
-			}
+		String returnString = ""+(int)number+"%";	
+		extraSpaces = 10 - returnString.length();
+		for (int j = 0; j < extraSpaces; j++) {
+			returnString += " ";
 		}
-	
+		return returnString;
+			}
+		
 	public String fullCellText() {
 		number = number/100;
 		return ""+number+"";
--- TextExcel/src/textExcel/ValueCell.java
+++ TextExcel/src/textExcel/ValueCell.java
@@ -1,33 +1,41 @@
 package textExcel;
 
-public class ValueCell extends RealCells { //Current Sattuus when I input some thing long it pritns everything but the first number
-	private String userInput; //This will be used for the two methods thatr eturn strings
+public class ValueCell extends RealCells { // Current Sattuus when I input some thing long it pritns everything but the
+											// first number
+	private String userInput; // This will be used for the two methods thatr eturn strings
 	private double vaule;
+
 	public ValueCell(String userInput) {
 		super(userInput);
 		this.userInput = userInput;
 		vaule = Double.parseDouble(userInput);
 	}
-	public double getDouble() { //it returns 12 like 12.0
+
+	public double getDouble() { // it returns 12 like 12.0
 		return vaule;
 	}
+
 	public String abbreviatedCellText() {
 		int extraSpaces = 0; // To count how many spaces I need to add
 		String returnString = userInput;
-		System.out.println(userInput);
-		System.out.println(vaule);
+		System.out.println(returnString);
+		if (!returnString.contains(".")) {
+			returnString += ".0";
+		}
+		System.out.println(returnString);
 		if (userInput.length() >= 10) {
 			return returnString.substring(0, 10);
 		} else {
-			extraSpaces = 10 - userInput.length();
+			extraSpaces = 10 - returnString.length();
 			for (int j = 0; j < extraSpaces; j++) {
 				returnString += " ";
 			}
 			return returnString;
 		}
 	}
-	public String fullCellText() { //I am printing the whole String that was passed in 
-			return ""+vaule+"";
-		}
-	}
 
+	public String fullCellText() { // I am printing the whole String that was passed in
+		return "" + vaule + "";
+	}
+	
+}
